9276fe2038f4ce349a45bee8919179f13c6a546e
Salome Abigail <salomeabigail9@gmail.com>
2024-08-12 20:20:56 +0300

    updated react-hooks-router-routes-lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
+
 function NavBar() {
   return (
     <nav className="navbar">
-    
+   <NavLink to={"/"} exact="true">Home</NavLink>
+   <NavLink to="/directors">Directors</NavLink>
+   <NavLink to="/actors">Actors</NavLink>
     </nav>
     );
 };
